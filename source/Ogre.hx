cd4883359a666e75c542eee49f1d6a54a9350897
Robert Eldredge <reldredge10@gmail.com>
2015-10-10 19:40:59 -0400

    fixed merge error on ogre.hx

--- source/Ogre.hx
+++ source/Ogre.hx
@@ -3,12 +3,9 @@ import flixel.FlxSprite;
 import flixel.system.scaleModes.FillScaleMode;
 import flixel.util.FlxColor;
 import flixel.FlxG;
-<<<<<<< HEAD
-=======
 import flixel.FlxObject;
 import flixel.util.FlxPoint;
 import flixel.util.FlxVelocity;
->>>>>>> 83752a6a0a07914b9d8e84e41d1743f4fcc8c044
 
 /**
  * ...
@@ -16,49 +13,7 @@ import flixel.util.FlxVelocity;
  */
 class Ogre extends FlxSprite  
 {
-<<<<<<< HEAD
 
-	public var maxSpeed:Float = 200;
-	//define AI variables
-	
-	
-	public function new(X:Float=0, Y:Float=0) {
-		super(X, Y);
-		
-		//Load Ogre art
-		makeGraphic(256, 256, FlxColor.FUCHSIA); //replace with Ogre Art
-		
-		//horizontal drag
-		drag.x = 450;	
-	}
-	
-	override public function update():Void {
-		movement();
-		super.update();
-	}
-	
-	private function movement():Void {
-		//define variables checking left/right movement
-		var _left:Bool = false;
-		var _right:Bool = false;
-		//check if left or right keys are pressed
-		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
-		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
-		
-		//cannot move left and right at the same time
-		if (_left && _right) _left = _right = false;
-		//change speed and direction
-		if (_left) {
-			velocity.x = -maxSpeed;
-		}
-		else if (_right) {
-				velocity.x = maxSpeed;
-		}
-	}
-	
-}
-=======
-	
 	public var maxSpeed:Float = 200;
 	public var centerX:Float;
 	public var centerY:Float;
@@ -183,4 +138,3 @@ class FSM {
              activeState();
      }
 }
->>>>>>> 83752a6a0a07914b9d8e84e41d1743f4fcc8c044
