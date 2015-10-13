7f400bea1b20eef21921248e73eb751c3d3d2405
Michael Dewey <deweym@rpi.edu>
2015-10-12 21:37:53 -0400

    Added interactivity to town people

--- source/TownPerson.hx
+++ source/TownPerson.hx
@@ -10,7 +10,8 @@ class TownPerson extends FlxSprite
 	var walkFrames:Array<Array<Int>> = [[0, 6], [12, 18]];
 	var topFrames:Array<Int> = [1, 7, 13];
 	var bottomFrames:Array<Int> = [3, 19];
-	var hairFrames:Array<Int> = [3,4,5,8,9,10,11,14,15,16,17,21,22,23];
+	var hairFrames:Array<Int> = [3, 4, 5, 8, 9, 10, 11, 14, 15, 16, 17, 21, 22, 23];
+	var hairIndex:Int;
 
 	var pos_x:Int;
 	var pos_y:Int;
@@ -19,6 +20,10 @@ class TownPerson extends FlxSprite
 	var hair_sprite:FlxSprite;
 	var top_sprite:FlxSprite;
 	var bottom_sprite:FlxSprite;
+	var sparkle_sprite:FlxSprite;
+	var smokeSprite:FlxSprite;
+	var isPoof:Bool;
+	public var isCut:Bool;
 
 	public function new(pos_x:Int, pos_y:Int) 
 	{
@@ -30,13 +35,15 @@ class TownPerson extends FlxSprite
 		this.animation.add("idle", walkFrames[FlxRandom.intRanged(0, walkFrames.length-1)], 8, true);
 		this.animation.play("idle");
 		this.setPosition(pos_x, pos_y);
+		isCut = false;
 		
 		spriteGroup = new FlxGroup();
 		
 		hair_sprite = new FlxSprite();
 		hair_sprite.loadGraphic(resLocation, true, 64, 96);
 		hair_sprite.setPosition(pos_x, pos_y);
-		hair_sprite.animation.add("idle", [hairFrames[FlxRandom.intRanged(0, hairFrames.length-1)]], 1, true);
+		hairIndex = FlxRandom.intRanged(0, hairFrames.length - 1);
+		hair_sprite.animation.add("idle", [hairFrames[hairIndex]], 1, true);
 		hair_sprite.animation.play("idle");
 		
 		top_sprite = new FlxSprite();
@@ -52,16 +59,64 @@ class TownPerson extends FlxSprite
 		bottom_sprite.animation.add("idle", [bottomFrames[FlxRandom.intRanged(0, bottomFrames.length-1)]], 1, true);
 		bottom_sprite.animation.play("idle");
 		
+		sparkle_sprite = new FlxSprite();
+		/*sparkle_sprite.loadGraphic("assets/images/Characters/Main/sparkle.png", true, 96, 96);
+		sparkle_sprite.setPosition(pos_x, pos_y);
+		sparkle_sprite.animation.add("sparkle", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 28, true);
+		sparkle_sprite.alpha = 0;*/
+		
+		smokeSprite = new FlxSprite();
+		smokeSprite.loadGraphic("assets/images/Characters/Main/smoke.png", true, 96, 96);
+		smokeSprite.setPosition(pos_x-16, pos_y-10);
+		smokeSprite.animation.add("poof", [0,1,2,3,4,5,6,7,8,9,10,11,12,13], 28, false);
+		smokeSprite.alpha = 0;
+		isPoof = false;
 		
 		spriteGroup.add(this);
 		spriteGroup.add(top_sprite);
 		spriteGroup.add(bottom_sprite);
 		spriteGroup.add(hair_sprite);
+		spriteGroup.add(sparkle_sprite);
+		spriteGroup.add(smokeSprite);
 	}
 	
 	override public function update():Void
 	{
+		if (isPoof && smokeSprite.animation.finished) endPoof();
+		
 		super.update();
+		
+	}
+	
+	public function cutHair() {
+		// hair is now cut
+		isCut = true;
+		
+		// change hair to different style
+		hairIndex = FlxRandom.intRanged(0, hairFrames.length - 1,[hairIndex]);
+		hair_sprite.animation.add("idle2", [hairFrames[hairIndex]], 1, true);
+		hair_sprite.animation.play("idle2");
+		
+		// do poof animation
+		startPoof();
+	}
+	
+	public function startPoof():Void {
+		isPoof = true;
+		smokeSprite.alpha = 1;
+		smokeSprite.animation.play("poof");
+	}
+	
+	public function endPoof():Void {
+		// end poof
+		isPoof = false;
+		
+		// make smoke invis
+		smokeSprite.alpha = 0;
+		
+		// run sparkle animation
+		sparkle_sprite.alpha = 1;
+		sparkle_sprite.animation.play("sparkle");
 		
 	}
 	
